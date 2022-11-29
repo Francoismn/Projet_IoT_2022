6ff585f2f0325610089898674c10666abc11636f
Francoismn <113969158+Francoismn@users.noreply.github.com>
2022-11-29 20:17:41 +0100

    Add files via upload

---
+++ Projet IOT/assets/js/main.js
@@ -0,0 +1,23 @@
+/*
+
+LIEN NE FONCTIONNE PAS, MIS DIRECTEMENT DANS MAIN.HTML
+
+
+window.onload = () =>{
+var hUser1 = document.getElementById("hUser1"); //bonjour
+var hUser2 = document.getElementById('hUser2'); //bonsoir
+var heure = new Date().getHours();
+      
+if (heure = 10) {
+    hUser1.style.display = "block";
+    hUser2.style.display = "none";
+} else{
+    hUser1.style.display = "none";
+    hUser2.style.display = "block";
+}
+  
+
+
+
+}
+*/
